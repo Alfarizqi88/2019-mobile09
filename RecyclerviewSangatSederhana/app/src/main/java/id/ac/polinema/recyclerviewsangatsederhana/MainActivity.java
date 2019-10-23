2c68a0ad1763535adef8f009fbf70b892318c09a
Asus <alfarizqi92@gmail.com>
2019-10-23 16:39:12 +0700

    Membuat RecyclerView Sangat Sederhana

--- RecyclerviewSangatSederhana/app/src/main/java/id/ac/polinema/recyclerviewsangatsederhana/MainActivity.java
+++ RecyclerviewSangatSederhana/app/src/main/java/id/ac/polinema/recyclerviewsangatsederhana/MainActivity.java
@@ -1,14 +1,33 @@
 package id.ac.polinema.recyclerviewsangatsederhana;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+import id.ac.polinema.recyclerviewsangatsederhana.adapters.SuperHeroAdapters;
+import id.ac.polinema.recyclerviewsangatsederhana.models.SuperHero;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView rvSuperhero;
+    List<SuperHero> superHeroList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rvSuperhero = findViewById(R.id.rvSuperHero);
+        SuperHero hero=new SuperHero("Petruk");
+        superHeroList.add(hero);
+        hero = new SuperHero("Gareng");
+        superHeroList.add(hero);
+
+        SuperHeroAdapters superHeroAdapters= new SuperHeroAdapters(superHeroList);
+
+        rvSuperhero.setAdapter(superHeroAdapters);
+        rvSuperhero.setLayoutManager(new LinearLayoutManager(this));
     }
 }
